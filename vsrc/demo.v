3ea421f9e179f37537641afa95263c83d637337d
EPTansuo <han@eptansuo.163.com>
2024-04-09 16:40:50 +0800

    Modify demo

--- vsrc/demo.v
+++ vsrc/demo.v
@@ -1,24 +1,24 @@
 `timescale 1ns/1ps
 
 module demo(
-	input clk,
-	input rst,
-	input wire a,
-	output reg w	
-);
+        input clk,
+        input rst,
+        input wire a,
+        output reg w
+    );
 
-reg [2:0] buffer; //要检测的序列长度为3
+    reg [2:0] buffer; //要检测的序列长度为3
 
-always @(posedge clk) begin 
-		if(rst) begin
-			buffer <= 3'b0;
-			w <= 1'b0;
-		end
-		else begin 
-			buffer <= {buffer[1:0], a};
+    always @(posedge clk) begin
+        if(rst) begin
+            buffer <= 3'b0;
+            w <= 1'b0;
+        end
+        else begin
+            buffer <= {buffer[1:0], a};
 
-		   //相当于 w = ( 更新后的buffer == 3'b101)
-			w <=  (buffer[1] & (~buffer[0]) & a);
-		end
-end
+            //相当于 w = ( 更新后的buffer == 3'b101)
+            w <=  (buffer[1] & (~buffer[0]) & a);
+        end
+    end
 endmodule
